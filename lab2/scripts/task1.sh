1eff3894b9134daf5efa81bde5c4c203d53864a7
Egor Linnikov <98ryba4ok@MacBook-Pro-Egor.local>
2025-06-23 23:31:52 +0300

    labs_fix

--- lab2/scripts/task1.sh
+++ lab2/scripts/task1.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 read -p "Введите размер доски (например, 8): " size
@@ -6,13 +7,12 @@ if ! [[ $size =~ ^[0-9]+$ ]] || [ "$size" -le 0 ]; then
     exit 1
 fi
 
-WHITE_BG="\e[47m  \e[0m"
-BLACK_BG="\e[40m  \e[0m"
+WHITE_BG="\033[47m  \033[0m"
+BLACK_BG="\033[40m  \033[0m"
 
 for ((i=0; i < size; i++)); do
     for ((j=0; j < size; j++)); do
-        if $(( (i+j)%2==0 )) &>/dev/null
-        then
+        if (( (i + j) % 2 == 0 )); then
             echo -ne "$WHITE_BG"
         else
             echo -ne "$BLACK_BG"
